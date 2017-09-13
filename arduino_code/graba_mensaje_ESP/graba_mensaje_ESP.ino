216d3feedd43d3de8074153c281866695a38b043
borjiso <borjiso@hotmail.com>
2017-09-13 20:18:08 +0200

    URL Encoding

--- arduino_code/graba_mensaje_ESP/graba_mensaje_ESP.ino
+++ arduino_code/graba_mensaje_ESP/graba_mensaje_ESP.ino
@@ -65,7 +65,8 @@ void loop()
   }
   do {
     char caracter_leido = Serial.read();
-    if (caracter_leido == ' ') caracter_leido = '_';
+    if (caracter_leido == ' ') caracter_leido = '%20';
+    if (caracter_leido == '!') caracter_ledio = '%21';
     if (caracter_leido == '\r') continue;
     if (caracter_leido == '\n') continue;
     mensaje += caracter_leido;
